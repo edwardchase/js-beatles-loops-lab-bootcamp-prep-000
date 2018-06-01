d8d943495a06a46be306ccd5c2589d6f1ed8bf7e
Edward Mixon <edwardchase@gatech.edu>
2018-06-01 19:05:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ function johnLennonFacts(facts) {
 }
 
 
+function iLoveTheBeatles(number) {
+  var count = 0;
+  var array = [];
+  do {
+    array[count] = 'I love the Beatles!';
+    count = count+1;
+} while (number < 15) {
+   array[count] = 'I love the Beatles!';
+    count = count + 1;
+    number = number + 1;
+    return array;
+}
+}
